5ca002d109e62ce6c26f0a780b40b7e70ddfaf46
Emiliano Rotta <emilianorottaf@gmail.com>
2024-12-18 20:02:22 -0300

    consigna ejercicio uno

---
+++ ejercicio-uno.js
@@ -0,0 +1,13 @@
+// Consigna
+// Crea una API REST básica utilizando Express en Node.js que permita practicar los códigos de estado HTTP más comunes. Implementa las siguientes rutas y funcionalidades:
+
+// Requisitos:
+// Rutas principales:
+// GET /success
+// Responde con un código de estado 200 OK y un mensaje indicando que la solicitud fue exitosa.
+// POST /created
+// Responde con un código de estado 201 Created y un mensaje indicando que un recurso fue creado.
+// GET /not-found
+// Responde con un código de estado 404 Not Found y un mensaje indicando que el recurso no fue encontrado.
+// GET /error
+// Responde con un código de estado 500 Internal Server Error y un mensaje indicando que hubo un error en el servidor.
